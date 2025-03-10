a9e2428315cfeeb7636ed0e8caad79d678dd4970
JustinPras <justinponpras@gmail.com>
2025-03-10 18:15:25 +1100

    refactor code

--- main.go
+++ main.go
@@ -14,76 +14,32 @@ func main() {
 		log.Fatalf("error reading config: %v", err)
 	}
 
-	s := state{
-		cfgPtr: &cfg,
+	programState := state{
+		cfg: &cfg,
 	}
 
-	//create map of handler functions
-	handlerFunctions := make(map[string]func(*state, command) error)
-
-	commands := commands{
-		handlerFunctions: handlerFunctions,
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
 	}
 
-	commands.register("login", handlerLogin)
+	cmds.register("login", handlerLogin)
 	
 
 	if len(os.Args) < 2 {
-		log.Fatalf("no arguments provided")
+		log.Fatalf("Usage: cli <command> [args...]")
 	}
 
 	cmd := command {
-		name: os.Args[1],
-		args: os.Args[2:],
+		Name: os.Args[1],
+		Args: os.Args[2:],
 	}
 
-	err = commands.run(&s, cmd)
+	err = cmds.run(&programState, cmd)
 	if err != nil {
 		log.Fatalf("error running command: %s", err)
 	}
-	
 }
 
 type state struct {
-	cfgPtr	*config.Config
+	cfg	*config.Config
 }
-
-type command struct {
-	name	string
-	args	[]string
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		return fmt.Errorf("the login handler expects a single argument, the username")
-	}
-
-	username := cmd.args[0]
-
-	s.cfgPtr.SetUser(username)
-	fmt.Println("User has been set")
-	return nil
-}
-
-type commands struct {
-	handlerFunctions map[string]func(*state, command) error
-}
-
-func (c *commands) register(name string, f func(*state, command) error) {
-	c.handlerFunctions[name] = f
-}
-
-func (c *commands) run(s *state, cmd command) error {
-	f, ok := c.handlerFunctions[cmd.name]
-	if !ok {
-		return fmt.Errorf("Command does not exist")
-	}
-
-	err := f(s, cmd)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
