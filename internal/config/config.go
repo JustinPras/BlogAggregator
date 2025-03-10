79ec58fdabdb56a4fb1564f1f51f29f38f7e2dd7
JustinPras <justinponpras@gmail.com>
2025-03-10 13:42:33 +1100

    fixed config files

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,42 +3,37 @@ package config
 import (
 	"os"
 	"encoding/json"
-	"fmt"
 )
 
-const configFileName = ".gatorconfig.json"
+const configFileName = "/gatorconfig.json"
 
 type Config struct {
-    db_url string `json:"db_url"`
-	current_user_name string `json:"current_user_name"`
+    DbURL string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
 }
 
 func Read() (Config, error) {
-	fmt.Println("Entering Read() function")
 	config := Config{}
 
 	configPath, err := getConfigFilePath()
 	if err != nil {
         return config, err
     }
-	fmt.Printf("Config File Path retrieved: %s\n", configPath)
 
 	data, err := os.ReadFile(configPath)
 	if err != nil {
 		return config, err
 	}
-	fmt.Println("Config File Read")
 
 	err = json.Unmarshal(data, &config)
 	if err != nil {
 		return config, err
 	}
-
 	return config, nil
 }
 
 func (c Config) SetUser(username string) {
-	c.current_user_name = username
+	c.CurrentUserName = username
 	write(c)
 }
 
@@ -47,7 +42,7 @@ func getConfigFilePath() (string, error) {
 	if err != nil {
 		return "", err
 	}
-	
+
 
 	return homeDir + configFileName, nil
 }
