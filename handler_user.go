99ea8e96f76861e7d1185d08bd83c16e2cdeeda6
JustinPras <justinponpras@gmail.com>
2025-03-11 15:21:21 +1100

    added list all users functionality

--- handler_user.go
+++ handler_user.go
@@ -61,3 +61,19 @@ func handlerLogin(s *state, cmd command) error {
 	fmt.Println("User switched successfully!")
 	return nil
 }
+
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't retrieve users: %w", err)
+	}
+
+	for _, user := range(users) {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %s (current)\n", user.Name)
+		} else {
+			fmt.Printf("* %s\n", user.Name)
+		}
+	}
+	return nil
+}
