ed6990ee9f1bc3a250834a3aca10811b0b0d775c
JustinPras <justinponpras@gmail.com>
2025-03-11 15:11:42 +1100

    added database reset fuctionality

--- handler_user.go
+++ handler_user.go
@@ -61,3 +61,13 @@ func handlerLogin(s *state, cmd command) error {
 	fmt.Println("User switched successfully!")
 	return nil
 }
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.Reset(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't reset database: %w", err)
+	}
+
+	fmt.Println("Database resetted successfully!")
+	return nil;
+}
