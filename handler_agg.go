840ae3dfc77628745038e0943053a044e7e1ce04
JustinPras <justinponpras@gmail.com>
2025-03-17 15:55:37 +1100

    added feed aggregation functionality

--- handler_agg.go
+++ handler_agg.go
@@ -3,15 +3,58 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
+	"database/sql"
+
+	"github.com/JustinPras/BlogAggregator/internal/database"
 )
 
 func handlerAggregator(s *state, cmd command) error {
-	rssFeed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return fmt.Errorf("couldn't fetch feed: %w", err)
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <duration>", cmd.Name)
 	}
 
-	fmt.Printf("RSS Feed: %+v\n", rssFeed)
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("Invalid duration entered: %w", err)
+	}
 
+	fmt.Printf("Collecting feeds every %v\n", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+
+	return nil
+}
+
+func scrapeFeeds(s *state) error {
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("Error getting next feed to fetch: %w", err)
+	}
+
+	lastFetchedAt := sql.NullTime{Time: time.Now(), Valid: true}
+
+	markFeedFetchedParams := database.MarkFeedFetchedParams {
+		LastFetchedAt:	lastFetchedAt,
+		UpdatedAt:		time.Now(),
+		ID:				feed.ID,
+	}
+
+	err = s.db.MarkFeedFetched(context.Background(), markFeedFetchedParams)
+	if err != nil {
+		return fmt.Errorf("Error marking feed as fetched: %w", err)
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		return fmt.Errorf("Error fetching feed: %w", err)
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Printf("* Title:     %s\n", item.Title)
+	}
 	return nil
 }
